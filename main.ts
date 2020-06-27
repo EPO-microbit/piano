5ad2a148cf0df1a9f5c1fa835bc25ea13f11905c
EPO-microbit <67168689+EPO-microbit@users.noreply.github.com>
2020-06-27 21:15:29 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    if (vol > 0) {
+        vol += 0 - 10
+        music.setVolume(vol)
+        basic.showString("" + (vol))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (vol < 150) {
+        vol += 0 + 10
+        music.setVolume(vol)
+        basic.showString("" + (vol))
+    }
+})
+let vol = 0
 for (let index = 0; index < 4; index++) {
     WSTouchPiano.TP_ShowRGB(
     WSTouchPiano.TP_SetRGB(255, 0, 0),
@@ -13,42 +28,40 @@ for (let index = 0; index < 4; index++) {
     )
 }
 let Color1 = 0
-let Color2 = 0
-let Color3 = 0
+vol = 50
 basic.forever(function () {
-    if (Color1 > 255) {
-        Color1 = 0
-    } else if (Color2 > 255) {
-        Color2 = 0
-    } else if (Color3 > 255) {
-        Color3 = 0
-    }
-    WSTouchPiano.TP_ShowRGB(
-    WSTouchPiano.TP_SetColor(RGB_COLOR.RED),
-    WSTouchPiano.TP_SetColor(RGB_COLOR.ORANGE),
-    WSTouchPiano.TP_SetColor(RGB_COLOR.YELLOW),
-    WSTouchPiano.TP_SetColor(RGB_COLOR.GREEN)
-    )
     if (WSTouchPiano.TP_Press(TP_PIANO.C)) {
         WSTouchPiano.TP_PlayMusic(262, music.beat(BeatFraction.Half))
-        Color1 += 50
-        Color2 += 50
-        Color3 += 50
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.RED)
     } else if (WSTouchPiano.TP_Press(TP_PIANO.D)) {
         WSTouchPiano.TP_PlayMusic(294, music.beat(BeatFraction.Half))
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.ORANGE)
     } else if (WSTouchPiano.TP_Press(TP_PIANO.E)) {
         WSTouchPiano.TP_PlayMusic(330, music.beat(BeatFraction.Half))
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.YELLOW)
     } else if (WSTouchPiano.TP_Press(TP_PIANO.F)) {
         WSTouchPiano.TP_PlayMusic(349, music.beat(BeatFraction.Half))
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.GREEN)
     } else if (WSTouchPiano.TP_Press(TP_PIANO.G)) {
         WSTouchPiano.TP_PlayMusic(392, music.beat(BeatFraction.Half))
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.BLUE)
     } else if (WSTouchPiano.TP_Press(TP_PIANO.A)) {
         WSTouchPiano.TP_PlayMusic(440, music.beat(BeatFraction.Half))
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.INDIGO)
     } else if (WSTouchPiano.TP_Press(TP_PIANO.B)) {
         WSTouchPiano.TP_PlayMusic(494, music.beat(BeatFraction.Half))
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.VIOLET)
     } else if (WSTouchPiano.TP_Press(TP_PIANO.C1)) {
         WSTouchPiano.TP_PlayMusic(523, music.beat(BeatFraction.Half))
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.PURPLE)
     } else if (WSTouchPiano.TP_Press(TP_PIANO.bB)) {
         music.playMelody("G B F A E C5 E G ", 120)
+        Color1 = WSTouchPiano.TP_SetColor(RGB_COLOR.WHITE)
     }
+    WSTouchPiano.TP_ShowRGB(
+    Color1,
+    Color1,
+    Color1,
+    Color1
+    )
 })
